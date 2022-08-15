ae82d73f6e319b3cfc3b81901a746b036b7bc0dd
Mario <marioskrlec222@gmail.com>
2022-08-15 11:02:12 +0200

    Added tests for argument validation

--- annie_test.go
+++ annie_test.go
@@ -32,6 +32,330 @@ var _ = Describe("Creating annie and closing", func() {
 	})
 })
 
+var _ = Describe("Argument validation", func() {
+	Context("should fail", func() {
+		It("when using CannotBeEmpty (empty argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.CannotBeEmpty()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using CannotBeEmpty (crowded argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.CannotBeEmpty("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsArray (empty argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.IsArray()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsArray (crowded argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.IsArray("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsMap (empty argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.IsMap()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsMap (crowded argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.IsMap("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsString (empty argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.IsString()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsString (crowded argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.IsString("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsNumeric (empty argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.IsNumeric()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsNumeric (crowded argument list)", func() {
+			ann, err := NewAnnie("test_base_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.IsNumeric("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsNumeric in node (empty argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").IsNumeric()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsNumeric (crowded argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").IsNumeric("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsString in node (empty argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").IsString()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsString (crowded argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").IsString("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsArray in node (empty argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").IsArray()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsArray (crowded argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").IsArray("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsMap in node (empty argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").IsMap()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using IsMap (crowded argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").IsMap("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using CannotBeEmpty in node (empty argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").CannotBeEmpty()
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+
+		It("when using CannotBeEmpty (crowded argument list)", func() {
+			ann, err := NewAnnie("test_complex_config.yml")
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(ann).ShouldNot(gomega.BeNil())
+
+			ann.StepInto("configuration").CannotBeEmpty("", "", "")
+
+			errs := ann.Errors()
+
+			gomega.Expect(errs).ShouldNot(gomega.BeEmpty())
+			gomega.Expect(errs).Should(gomega.HaveLen(1))
+			gomega.Expect(errs[0]).ShouldNot(gomega.BeNil())
+
+			ann.Close()
+		})
+	})
+})
+
 var _ = Describe("Failures and safety", func() {
 	It("should fail if stepping into primitive type node and ignore rest of them", func() {
 		ann, err := NewAnnie("test_complex_config.yml")
